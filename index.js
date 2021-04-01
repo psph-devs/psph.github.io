960d8fdc98153d0b7aa392dfa07e702c41bb12ac
dev.edzlazaro@outlook.com <dev.edzlazaro@outlook.com>
2021-04-01 17:27:32 +0800

    Added Leaflet.js as static map provider

--- index.js
+++ index.js
@@ -53,3 +53,15 @@ var navLinks = document.getElementsByClassName("nav-link");
     scrollTo(document.getElementById("contact-content"));
     document.getElementById("contact-link").classList.add("active-link")
   });
+
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoicHNwaC1kZXZzIiwiYSI6ImNrbXlvYW14czA1d20ycHBrd3c2cTB2eHcifQ.y3jPEQo_tl2ga0l8U9SCkw'
+}).addTo(mymap);
